f6d88b5de719a698be824889b26cfec71a7fca28
Aleksandr Tikhomirov <aleksandr.tikhomirov@1ex.com>
2023-08-04 15:26:28 +0700

    task is ready

---
+++ apps/web/public/src/factories/factories.module.js
@@ -0,0 +1,60 @@
+export const factoriesModule = angular
+  .module('factories', [])
+  .factory('Server', [
+    '$resource',
+    function ($resource) {
+      return $resource(
+        '/servers/:id',
+        { id: '@_id' },
+        {
+          start: {
+            url: '/servers/:id/start',
+            method: 'get',
+            isArray: false,
+          },
+          restart: {
+            url: '/servers/:id/restart',
+            method: 'get',
+            isArray: false,
+          },
+          stop: {
+            url: '/servers/:id/stop',
+            method: 'get',
+            isArray: false,
+          },
+        }
+      )
+    },
+  ])
+  .factory('Task', [
+    '$resource',
+    function ($resource) {
+      return $resource(
+        '/tasks/:id',
+        { id: '@_id' },
+        {
+          addRandom: {
+            url: '/tasks/add-random',
+            method: 'get',
+            isArray: false,
+          },
+        }
+      )
+    },
+  ])
+  .factory('Charts', [
+    '$resource',
+    function ($resource) {
+      return $resource(
+        '/charts',
+        {},
+        {
+          serverCharts: {
+            url: '/charts/:serverId',
+            method: 'get',
+            isArray: false,
+          },
+        }
+      )
+    },
+  ])
---
+++ config/default.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apps: {
+    web: {
+      port: 3000,
+    },
+  },
+  mongodb: 'mongodb://tikh:Y7FV82aWv6798mF6@89.208.85.128/Mobilon',
+}
